e4139e7d819c6ee762ba988c1407ac89e457d256
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-07-24 23:42:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function getBrowserDetail (){
+	let ua = navigator.userAgent;
+	// console.log('You are using', ua);
+	let displayDetail = document.body;
+	displayDetail.innerHTML = 'You are using '+ua;
+	// console.log(displayDetail);
+}
+
+getBrowserDetail();
